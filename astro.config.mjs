e3d3316630ae406adbe37740f102ec805a48b4c3
Matheus Almeida <matheusalmeida016@gmail.com>
2024-03-14 07:33:08 -0400

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import viteSvgr from "vite-plugin-svgr";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
   vite: {
-    plugins: [viteSvgr()],
+    plugins: [viteSvgr()]
   },
   output: "server",
+  adapter: netlify()
 });
